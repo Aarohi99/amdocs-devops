d523ae86a7ff45a9a37a1c13c11f08ac706ca4ba
Aarohi99 <jyotimsinha74@gmail.com>
2022-08-05 11:31:06 +0530

    The First Devops Project

---
+++ src/main/java/com/training/MyClass.java
@@ -0,0 +1,16 @@
+package com.training;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Devops!");
+		
+
+	}
+
+}
+
+//Git: Local Repository
+
+
+//Github: Remote Repository
